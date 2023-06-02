1d8087b1161aad457f331d591dcc4ae1c3c00bb2
manne0081@gmail.com <manne0081@gmail.com>
2023-06-02 08:38:06 +0200

    alt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { TeamMemberComponent } from './team-member/team-member.component';
-import { TeamMemberListComponent } from './team-member/team-member-list/team-member-list.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { QuicklinksComponent } from './quicklinks/quicklinks.component';
-import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ActionsComponent } from './actions/actions.component';
+
+import { HomeComponent } from './home/home.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TeamMemberComponent } from './team-member/team-member.component';
+import { TeamMemberListComponent } from './team-member/team-member-list/team-member-list.component';
 import { FolderComponent } from './folder/folder.component';
 import { FolderListComponent } from './folder/folder-list/folder-list.component';
-import { ActionsComponent } from './actions/actions.component';
 
 @NgModule({
   declarations: [
